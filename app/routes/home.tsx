c168c339fd7fbb00a6bee817811cae6194be8d7e
tarun <tarunreddy.b@enterpi.com>
2025-07-21 10:23:28 +0530

    authentication done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import {usePuterStore} from "~/lib/puter";
-import {Link, useNavigate} from "react-router";
-import {useEffect, useState} from "react";
+import { usePuterStore } from "~/lib/puter";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,40 +14,39 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {  auth } = usePuterStore();
+  const navigate = useNavigate();
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
-    <section className="main-section">
-      <div className="page-heading py-16">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        {/* {!loadingResumes && resumes?.length === 0 ? (
-            <h2>No resumes found. Upload your first resume to get feedback.</h2>
-        ): (
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
-        )} */}
-      </div>
-      {/* {loadingResumes && (
-          <div className="flex flex-col items-center justify-center">
-            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
-          </div>
-      )}
+        </div>
+      </section>
 
-      {!loadingResumes && resumes.length > 0 && (
+      {resumes.length > 0 && (
         <div className="resumes-section">
-          {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
+          {resumes.map((resume: any) => (
+            <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
-      )} */}
+      )}
 
-      {/* {!loadingResumes && resumes?.length === 0 && (
-          <div className="flex flex-col items-center justify-center mt-10 gap-4">
-            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
-              Upload Resume
-            </Link>
-          </div>
-      )} */}
-    </section>
-  </main>
+      <div className="flex flex-col items-center justify-center mt-10 gap-4">
+        <Link
+          to="/upload"
+          className="primary-button w-fit text-xl font-semibold"
+        >
+          Upload Resume
+        </Link>
+      </div>
+    </main>
+  );
 }
